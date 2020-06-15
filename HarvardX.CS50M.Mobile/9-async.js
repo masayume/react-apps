ab5f23f09c4a670715a1a37e05e2021d6de730c4
masayume <masayume.blog@gmail.com>
2020-06-15 21:20:46 +0200

    lesson 2

---
+++ HarvardX.CS50M.Mobile/9-async.js
@@ -0,0 +1,23 @@
+// javascript is single-threaded and synchronous (a while(true) hangs the browser)
+// there are async functions
+
+// Execution stack is like a deck of cards
+
+
+function printOne()  {
+    console.log('one')
+}
+
+function printTwo()  {
+    console.log('two')
+}
+
+function printThree()  {
+    console.log('three')
+}
+
+setTimeout(printOne, 1000)
+setTimeout(printTwo, 0)
+printThree()
+
+
